296c364a8abf940145b315f562e54e70f83f6e49
MAHA <105947006+Maha028@users.noreply.github.com>
2026-01-06 16:45:34 +0300

    Update setup.py

--- libs/utils/setup.py
+++ libs/utils/setup.py
@@ -4,16 +4,15 @@ from setuptools import setup, Extension
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 
+
 setup(
     name='nms_1d_cpu',
     ext_modules=[
         CppExtension(
-            name = 'nms_1d_cpu',
-            sources = ['./csrc/nms_cpu.cpp'],
+            name='nms_1d_cpu',
+            sources=[os.path.join(this_dir, 'csrc', 'nms_cpu.cpp')],
             extra_compile_args=['-fopenmp']
         )
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    cmdclass={'build_ext': BuildExtension}
 )
